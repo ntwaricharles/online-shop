7963732f2587d881fb4e340703ce1e68fad0d150
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-12 11:41:38 +0200

    fixed styles

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  // Property to control navigation visibility on mobile
+  isNavOpen = false;
 
+  // Method to toggle the navigation bar in mobile view
+  toggleNav() {
+    this.isNavOpen = !this.isNavOpen;
+  }
 }
